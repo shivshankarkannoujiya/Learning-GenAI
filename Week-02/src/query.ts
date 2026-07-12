18b8ed084c90b06ba3cc6c154641fc1fd10a2649
shivshankarkannoujiya <kannoujiyaji143@gmail.com>
2026-07-12 17:39:12 +0530

    feat: add query re-writting

--- Week-02/src/query.ts
+++ Week-02/src/query.ts
@@ -10,10 +10,39 @@ import { openaiClient } from './utils/openaiClient.js';
 import { vectorStore } from './utils/store.js';
 
 const query = async (userQuery: string) => {
+    const QUERY_SYSTEM_PROMPT = `
+        You are an expert Query Rewriting AI for a Retrieval-Augmented Generation (RAG) system.
+
+        Your task is to rewrite the user's query to maximize semantic retrieval from the vector database.
+
+        Rules:
+            - Preserve the original intent exactly.
+            - Do not answer the question.
+            - Do not introduce new facts or assumptions.
+            - Expand the query with relevant synonyms, alternative terminology, abbreviations, and domain-specific keywords when appropriate.
+            - Make implicit concepts explicit only if they are clearly implied by the user's query.
+            - Return only the rewritten query.
+            - Do not include explanations, notes, bullet points, or markdown.
+
+        User Query:
+        ${userQuery}
+    `;
+
+    const response = await openaiClient.chat.completions.create({
+        model: 'gpt-4o-mini',
+        messages: [
+            { role: 'system', content: QUERY_SYSTEM_PROMPT },
+            { role: 'user', content: userQuery },
+        ],
+    });
+
+    const enhancedQuery = response.choices[0]?.message.content;
+    console.log(enhancedQuery);
+
     const vectorRetriever = vectorStore.asRetriever({ k: 5 });
     const result = await vectorRetriever.invoke(userQuery);
 
-    console.log(result);
+    // console.log(result);
 
     const SYSTEM_PROMPT = `
         You are an expert in answering the user query based on the provided context about document.
@@ -33,16 +62,16 @@ const query = async (userQuery: string) => {
             .join('\n\n')}
     `;
 
-    console.log(SYSTEM_PROMPT);
+    // console.log(SYSTEM_PROMPT);
     const llmResponse = await openaiClient.chat.completions.create({
         model: 'gpt-4o-mini',
         messages: [
             { role: 'system', content: SYSTEM_PROMPT },
-            { role: 'user', content: userQuery },
+            { role: 'user', content: enhancedQuery! },
         ],
     });
 
     console.log('🐬', llmResponse.choices[0]?.message.content);
 };
 
-await query('What is fs module?');
+await query('How to create http server ?');
